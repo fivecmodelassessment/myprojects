b17b978e05e4c80a11d9fa36e1a6b19cce2e3ee5
dimitranov <g_dimitranov@abv.bg>
2017-09-09 20:04:59 +0300

    fdasfaf

--- PORTFOLIO_SITE/portfolio.js
+++ PORTFOLIO_SITE/portfolio.js
@@ -10,8 +10,8 @@ $(window).scroll(function  () {
 
 });
 
-$(".nav-toggle").click(function(event) {
-$(this).toggleClass("active");
+$(".navigation-hamburger").click(function(event) {
+$(".nav-toggle").toggleClass("active");
 	/* Act on the event */
 });
 var nav_switch = true;
@@ -20,17 +20,65 @@ function Side_nav_visible() {
 		$(".side-menu").css('width', '0px');
 		$(".navigation-hamburger").css('right', '0px');
 		$(".navigation-hamburger").css('position', 'relative');
+		$(".tr1").removeClass('tr1_an');
+		$(".tr2").removeClass('tr2_an');
+		$(".tr3").removeClass('tr3_an');
+		$(".tr4").removeClass('tr4_an');
+		$(".tr5").removeClass('tr5_an');
+		$(".tr6").removeClass('tr6_an');
+		$(".tr7").removeClass('tr7_an');
+		$(".tr8").removeClass('tr8_an');
+		$(".tr9").removeClass('tr9_an');
+		$(".tr10").removeClass('tr10_an');
 	}else{
 		$(".side-menu").css('width', '250px');
 		$(".navigation-hamburger").css('right', '60px');
 		$(".navigation-hamburger").css('position', 'fixed');
+
+		$(".tr1").addClass('tr1_an');
+		$(".tr2").addClass('tr2_an');
+		$(".tr3").addClass('tr3_an');
+		$(".tr4").addClass('tr4_an');
+		$(".tr5").addClass('tr5_an');
+		$(".tr6").addClass('tr6_an');
+		$(".tr7").addClass('tr7_an');
+		$(".tr8").addClass('tr8_an');
+		$(".tr9").addClass('tr9_an');
+		$(".tr10").addClass('tr10_an');
 	}
 nav_switch = !nav_switch;
 
 
 
 }
-$(".nav-toggle").click(Side_nav_visible);
+$(".navigation-hamburger").click(Side_nav_visible);
+
+
+$("#github_icon").hover(function() {
+	$(this).attr('src', 'images/giticon2.png');
+}, function() {
+	$(this).attr('src', 'images/giticon.png');
+});
+$("#facebook_icon").hover(function() {
+	$(this).attr('src', 'images/fbicon2.png');
+}, function() {
+	$(this).attr('src', 'images/fbicon.png');
+});
+$("#instagram_icon").hover(function() {
+	$(this).attr('src', 'images/igramicon2.png');
+}, function() {
+	$(this).attr('src', 'images/igramicon.png');
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
