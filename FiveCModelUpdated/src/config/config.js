55c590ff91b9695b18354ee1f1db214bf314a164
fivecmodelassessment <34347892+fivecmodelassessment@users.noreply.github.com>
2017-12-11 00:49:02 +0200

    Update config.js

--- FiveCModelUpdated/src/config/config.js
+++ FiveCModelUpdated/src/config/config.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app"
 import "firebase/database"
 
 export const DB_CONFIG_DATA = {
-    apiKey: "AIzaSyCzXlQi69M81BrFWE6VyxC9RPvjRQXVzJU",
-    authDomain: "fivecassesment.firebaseapp.com",
-    databaseURL: "https://fivecassesment.firebaseio.com",
-    projectId: "fivecassesment",
-    storageBucket: "fivecassesment.appspot.com",
-    messagingSenderId: "18851441588"
+    ....
   };
 
 
